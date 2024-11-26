4a6e354ba72a0f1c097a7600854aab96b5bf2526
mhakimsaputra17 <a1011201296@student.untan.ac.id>
2024-11-27 03:33:38 +0700

    commit 2

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,35 +8,11 @@ import Image from "next/image";
 const EmailSection = () => {
   const [emailSubmitted, setEmailSubmitted] = useState(false);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const data = {
-      email: e.target.email.value,
-      subject: e.target.subject.value,
-      message: e.target.message.value,
-    };
-    const JSONdata = JSON.stringify(data);
-    const endpoint = "/api/send";
-
-    // Form the request for sending data to the server.
-    const options = {
-      // The method is POST because we are sending data.
-      method: "POST",
-      // Tell the server we're sending JSON.
-      headers: {
-        "Content-Type": "application/json",
-      },
-      // Body of the request is the JSON data we created above.
-      body: JSONdata,
-    };
-
-    const response = await fetch(endpoint, options);
-    const resData = await response.json();
-
-    if (response.status === 200) {
-      console.log("Message sent.");
-      setEmailSubmitted(true);
-    }
+    // You can add any local logic here if needed
+    console.log("Form submitted locally.");
+    setEmailSubmitted(true);
   };
 
   return (
